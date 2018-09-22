fdca9cc1e84b683a6294838dd2c3287e2c6312c1
Svyatoslav Ryhel <clamor95@gmail.com>
2018-09-22 10:47:31 +0300

    mako: add 8GB model support
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

---
+++ mako/mako_repartition_8GB.sh
@@ -0,0 +1,40 @@
+#!/sbin/sh
+#
+# Copyright (C) 2018 Unlegacy Android Project
+# Copyright (C) 2018 Svyatoslav Ryhel
+#
+# Made for Mako 8GB
+#
+
+umount /system
+umount /data
+umount /sdcard
+umount /cache
+
+parted /dev/block/mmcblk0 <<EOF
+  rm 21
+  rm 22
+  rm 23
+  mkpart primary 159383552B 1732247551B
+  name 21 system
+  mkpart primary 1732247552B 1994391551B
+  name 22 vendor
+  mkpart primary 1994391552B 2099249151B
+  name 23 cache
+  mkpart primary 2099249152B 7817134079B
+  name 26 userdata
+  quit
+EOF
+
+make_ext4fs /dev/block/mmcblk0p21
+make_ext4fs /dev/block/mmcblk0p22
+make_ext4fs /dev/block/mmcblk0p23
+make_ext4fs /dev/block/mmcblk0p26
+
+ln -sf /dev/block/mmcblk0p21 system
+ln -sf /dev/block/mmcblk0p22 vendor
+ln -sf /dev/block/mmcblk0p23 cache
+ln -sf /dev/block/mmcblk0p26 userdata
+
+rm /sbin/parted
+reboot recovery
---
+++ mako/mako_stock_8GB.sh
@@ -0,0 +1,38 @@
+#!/sbin/sh
+#
+# Copyright (C) 2018 Unlegacy Android Project
+# Copyright (C) 2018 Svyatoslav Ryhel
+#
+# Made for Mako 8GB
+#
+
+umount /system
+umount /vendor
+umount /data
+umount /sdcard
+umount /cache
+
+parted /dev/block/mmcblk0 <<EOF
+  rm 21
+  rm 22
+  rm 23
+  rm 26
+  mkpart primary 159383552B 1040187391B
+  name 21 system
+  mkpart primary 1040187392B 1627389951B
+  name 22 cache
+  mkpart primary 1627389952B 7817134079B
+  name 23 userdata
+  quit
+EOF
+
+make_ext4fs /dev/block/mmcblk0p21
+make_ext4fs /dev/block/mmcblk0p22
+make_ext4fs /dev/block/mmcblk0p23
+
+ln -sf /dev/block/mmcblk0p21 system
+ln -sf /dev/block/mmcblk0p22 cache
+ln -sf /dev/block/mmcblk0p23 userdata
+
+rm /sbin/parted
+reboot recovery
