52847cb7df4d4fe13c75e8f7a1b6ebfba186b269
Svyatoslav Ryhel <clamor95@gmail.com>
2018-11-23 22:14:38 +0200

    cl2n: initial bringup
    Add clean minimum needed for TWRP build.
    
    Change-Id: Ie3e3a8cd871eb2afe6ec955c2f84dc7be199435a
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

---
+++ omni_a2109.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2018 Unlegacy Android Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit some common omni stuff.
+$(call inherit-product-if-exists, vendor/omni/config/common.mk)
+
+# Inherit full-base
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from a2109 device
+$(call inherit-product, device/lenovo/a2109/device.mk)
+
+# Setup device specific product configuration.
+PRODUCT_NAME := omni_a2109
+PRODUCT_DEVICE := a2109
+PRODUCT_BRAND := Ideatab
+PRODUCT_MODEL := A2109A
+PRODUCT_MANUFACTURER := Lenovo
